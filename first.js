8e3241f5b388394489172cfdcd96a49daf462afc
Begari-Praveen <praveenbegari09@gmail.com>
2026-04-02 23:01:28 +0530

    updated

---
+++ first.js
@@ -0,0 +1,37 @@
+console.log("Hello , World");
+
+let a = 10;
+var b=20;
+const c= 20;
+ a=30;
+ b=40;
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+function add(a,b){
+    if(a>b){
+        var c = 20;
+        console.log(a+b+c);
+    }
+    console.log(c);
+}
+
+add(50,20);
+
+const num = "hii";
+console.log(typeof num);
+
+let a1= 5;
+
+console.log(a1++); // 5 (then becomes 6)
+console.log(++a1); // 7
+
+console.log(a1--);
+console.log(--a1);
+let msg = `Hello
+This is line 2
+This is line 3`;
+
+console.log(msg);
